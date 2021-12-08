5a4c24678f9b2499cbae7b56a21c0c45430e8169
Andera Bridger <abridger@oa-g3.msk.21-school.ru>
2021-12-08 18:58:34 +0300

    correct everyone_ate in bonus part

--- philo_bonus/ft_eat_bonus.c
+++ philo_bonus/ft_eat_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abridger <abridger@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 17:04:42 by abridger          #+#    #+#             */
-/*   Updated: 2021/12/03 17:14:16 by abridger         ###   ########.fr       */
+/*   Updated: 2021/12/08 18:53:28 by abridger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,10 @@ void	philo_eat(t_philo *philo)
 	philo->prev_time = philo->check_time;
 	philo->check_time = get_timestamp();
 	philo_print(philo, 2);
+	check_time(philo, 1);
+	philo_put_forks(philo);
 	philo->nb_eat++;
 	if (philo->data->nb_times_eat > 0
 		&& philo->nb_eat == philo->data->nb_times_eat)
 		sem_post(philo->data->sem_meal);
-	check_time(philo, 1);
-	philo_put_forks(philo);
 }
